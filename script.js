27fd5b8c3b4d8209d6d9c0a28c4ba3f662b26667
machine <analyticrye@gmail.com>
2020-09-07 20:14:49 -0400

    connected to API

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello");
+//Unsplash API
+const count = 10;
+const apiKey = "q8ZKvVsNdMzBMND8fuz8jTw6IXAHPireJ4jlMfuhBoA";
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+//Get photos from Unsplash API
+const getPhotos = async () => {
+  try {
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {}
+};
+
+getPhotos();
